574337be42d034eafa9c754f3569c6e2845eac0c
napoknot21 <kinbrian5@hotmail.com>
2022-10-30 11:27:45 +0100

    graphs, network and web-client adds

---
+++ graphs/java/Graph.java
@@ -0,0 +1,12 @@
+public class Graph {
+      
+    public static void main (String[] args) {
+        int a = 0;
+        while (a != 1) {
+            
+        }
+    }
+
+
+
+}
---
+++ graphs/java/Vertex.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Vertex {
+  
+  private string data;
+  private ArrayList<Edges> edges;
+  
+  /**
+   * Constructor
+   */
+  public Vertex (String inputData) {
+    this.data = inputData;
+    this.edges = new ArrayList<Edge>();
+  }
+
+  public void addEdge (Vertex endVertex, Integer weight) {
+
+  }
+
+  public static void main (String[] args) {
+
+  }
+
+
+}
